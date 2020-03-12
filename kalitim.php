edcbd2c81a83a21496f9398540f0df35f2e7bc66
Nursel ALTIN <nurselaltin.na@gmail.com>
2020-03-12 11:44:07 +0300

    staticeklendi

--- kalitim.php
+++ kalitim.php
@@ -1,4 +1,31 @@
 <?php
+//Static method kullanımı: Sınıftan bir nesne yaratmadan o methodu kullanılmasını ve bir çok nesnenin çağırılmasına olanak verir.Performansı arttırmak amacıyla kullanılır.
+  /*Ancak dikkat edilmesi gereken bir kaç husu var
+  1) Method içinde çağırılacak nesnelerinde başta  static olarak tanımlanması gerekir.
+  2)Bu nesneler çağırılırken self veya parent kullanılır. */
+class staticDeneme{
+
+    static $filename;
+    static function method($filename,$text){
+        //self
+        //parent
+        self::$filename = $filename;
+
+        $file = fopen($filename,'w+');//$filename isimli dosyayı aç yoksa oluştur(w+)
+        fwrite($file,$text);
+        fclose($file);
+        return file_get_contents($filename) ;
+    }
+
+}
+
+echo staticDeneme::method('a.txt','nursel altin');
+
+
+
+
+
+
 
 class a{
     public   function  test(){
@@ -10,11 +37,10 @@ class a{
 class  b extends a{
 
     //final: metodun,sınıfın  kalıtılmasını izin vermez.Büyük projelerde,birden çok kişiyle geliştirme yapılırken kullanılır.
-      final public  function  test(){
+      /*final */ public  function  test(){
             return 'b:test';
          }
 }
-
 class  c  extends  b{
     public  function  test(){
         return 'c:test';
@@ -35,4 +61,4 @@ class  c  extends  b{
 
 
 $nesne  = new  c();
-print_r($nesne->testleriGetir());
+//print_r($nesne->testleriGetir());
